4172009eed400df8ed7a446e4c7856fb9aa9f13a
awesome-partial-8458 <ningelvis@gmail.com>
2019-12-16 18:52:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,9 +20,8 @@ function add2(n) {
 }
 
 var funkyFunction = function() {
-  return function() {
+ {
     return "FUNKY!"
-  }
 }
 var theFunk = funkyFunction()
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
